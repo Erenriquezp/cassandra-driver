949041e3c8333d3d9f689498933183c90578125a
Erenriquezp <erenriquezp@uce.edu.ec>
2024-08-02 16:46:35 -0500

    Inicializacion del proyecto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         CassandraConnector connector = new CassandraConnector();
-        connector.connect("127.0.0.1", 9042, "Tokyo", "ks_results");
+        connector.connect("127.0.0.1", 9042, "Hokkaido", "ks_results");
 
         CassandraCRUD crud = new CassandraCRUD(connector.getSession());
         ExcelToCassandra excelUploader = new ExcelToCassandra(crud);
@@ -21,8 +21,16 @@ public class Main {
                 "neutral text, " +
                 "PRIMARY KEY (date, home_team, away_team))");
 
+        // Medir el tiempo de carga de datos
+        long startTime = System.currentTimeMillis();
+
         // Subir datos desde el archivo de Excel
         excelUploader.uploadExcelData("C:\\Users\\LENOVO\\IntelliJ_IDEA\\Java\\Date_Base\\cassandra-driver\\src\\main\\java\\org\\example\\results.xlsx", "football_results");
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        System.out.println("Tiempo de carga de datos: " + duration + " ms");
 
         // Leer datos
         crud.read("football_results", "1872-11-30");
